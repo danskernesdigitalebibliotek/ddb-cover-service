dc5120f2a279552e77cdf18706d947b0136d5b71
Troels Ugilt Jensen <tujensen@gmail.com>
2019-11-26 07:10:05 +0100

    DDBTEAM-306: Fixed issues with migrations and fixtures

---
+++ src/Migrations/Version20191125142443.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191125142443 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE source DROP FOREIGN KEY FK_5F8A7F73F603EE73');
+        $this->addSql('ALTER TABLE vendor CHANGE id id INT NOT NULL');
+        $this->addSql('ALTER TABLE source ADD CONSTRAINT FK_5F8A7F73F603EE73 FOREIGN KEY (vendor_id) REFERENCES vendor (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE vendor CHANGE id id INT AUTO_INCREMENT NOT NULL');
+    }
+}
