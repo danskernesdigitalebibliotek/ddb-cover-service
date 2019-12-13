55d18638a249a489f26a2b91bd50c689dbf382b8
Jesper Kristensen <cableman@linuxdev.dk>
2019-12-13 10:00:33 +0100

    Core: version 1.1.1

--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-(cd php7.3-fpm && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/php7.3-fpm:1.1.0 .)
-(cd cover-service && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service:1.1.0 .)
-(cd nginx && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx:1.1.0 .)
-(cd cover-service-jobs && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service-jobs:1.1.0 .)
+(cd php7.3-fpm && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/php7.3-fpm:1.1.1 .)
+(cd cover-service && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service:1.1.1 .)
+(cd nginx && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx:1.1.1 .)
+(cd cover-service-jobs && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service-jobs:1.1.1 .)
 
-docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/php7.3-fpm:1.1.0
-docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service:1.1.0
-docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx:1.1.0
-docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service-jobs:1.1.0
+docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/php7.3-fpm:1.1.1
+docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service:1.1.1
+docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx:1.1.1
+docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service-jobs:1.1.1
