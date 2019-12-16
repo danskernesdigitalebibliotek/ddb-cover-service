0f496eac30171d8cd33f205b59785e257415b7d7
Jesper Kristensen <cableman@linuxdev.dk>
2019-12-16 13:45:56 +0100

    DDBTEAM-347: Es curl test script

--- public/curl.php
+++ public/curl.php
@@ -1,10 +1,10 @@
 <?php
 
-$query = '{"query":{"bool":{"should":[{"bool":{"must":[{"terms":{"isIdentifier":["870970-basis:29506914","870970-basis:29506906","870970-basis:22506306"]}},{"term":{"isType":{"value":"pid","boost":1}}}]}}]}}}';
+$query = '{"query":{"bool":{"should":[{"term":{"isIdentifier":{"value":"870970-basis:47234727","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:28240562","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:28999410","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:51376404","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:52060125","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:50823539","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:29668485","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:54989393","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:28240627","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:50712192","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:29590311","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:51342836","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:29630461","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:53999204","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:27005306","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:52626951","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:51323599","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:50828905","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:52126851","boost":1}}},{"term":{"isIdentifier":{"value":"870970-basis:52028973","boost":1}}}]}},"size":20}';
 
 
-$ch = curl_init($_GET['url'] . '/app_dev/search/_search');
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+$ch = curl_init($_GET['url']);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
