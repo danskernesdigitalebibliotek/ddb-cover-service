99fbff3b20b3ed9ab87d709d9c4d220bb43269f4
Ture Gjørup <ture@ignatz.dk>
2019-12-16 12:37:37 +0100

    DDBTEAM-330: The Movie Database Vendor

---
+++ src/Service/VendorService/TheMovieDatabase/EventSubscriber/ResultEventSubscriber.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @file
+ */
+
+namespace App\Service\VendorService\TheMovieDatabase\EventSubscriber;
+
+use App\Service\VendorService\TheMovieDatabase\Message\ApiSearchMessage;
+use App\Service\VendorService\TheMovieDatabase\Event\ResultEvent;
+use App\Utils\Message\ProcessMessage;
+use App\Utils\Types\VendorState;
+use Enqueue\Client\ProducerInterface;
+use Enqueue\Util\JSON;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class VendorEventSubscriber.
+ */
+class ResultEventSubscriber implements EventSubscriberInterface
+{
+    private $producer;
+
+    /**
+     * VendorEventSubscriber constructor.
+     *
+     * @param producerInterface $producer
+     *   Queue producer to send messages (jobs)
+     */
+    public function __construct(ProducerInterface $producer)
+    {
+        $this->producer = $producer;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * Defines the events that we subscribes to.
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            ResultEvent::NAME => 'onDataWellSearchEvent',
+        ];
+    }
+
+    /**
+     * Event handler.
+     *
+     * @param ResultEvent $event
+     */
+    public function onDataWellSearchEvent(ResultEvent $event)
+    {
+        $results = $event->getResults();
+
+        foreach ($results as $pid => $meta) {
+            $message = new ApiSearchMessage($event->getVendorId(), $event->getIdentifierType(), $pid, $meta['title'], $meta['date']);
+
+            $this->producer->sendEvent('ApiSearchTopic', JSON::encode($message));
+        }
+    }
+}
---
+++ src/Service/VendorService/TheMovieDatabase/TheMovieDatabaseVendorService.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * @file
+ * Use a library's data well access to get comic+ covers.
+ */
+
+namespace App\Service\VendorService\TheMovieDatabase;
+
+use App\Exception\IllegalVendorServiceException;
+use App\Exception\UnknownVendorServiceException;
+use App\Service\VendorService\AbstractBaseVendorService;
+use App\Service\VendorService\ProgressBarTrait;
+use App\Service\VendorService\TheMovieDatabase\Event\ResultEvent;
+use App\Utils\Message\VendorImportResultMessage;
+use App\Utils\Types\IdentifierType;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Class DataWellVendorService.
+ */
+class TheMovieDatabaseVendorService extends AbstractBaseVendorService
+{
+    use ProgressBarTrait;
+
+    protected const VENDOR_ID = 6;
+
+    private $dataWell;
+    private $api;
+
+    /**
+     * DataWellVendorService constructor.
+     *
+     * @param EventDispatcherInterface $eventDispatcher
+     * @param EntityManagerInterface $entityManager
+     * @param LoggerInterface $statsLogger
+     * @param TheMovieDatabaseSearchService $dataWell
+     */
+    public function __construct(EventDispatcherInterface $eventDispatcher, EntityManagerInterface $entityManager,
+                              LoggerInterface $statsLogger, TheMovieDatabaseSearchService $dataWell, TheMovieDatabaseApiService $api)
+    {
+        parent::__construct($eventDispatcher, $entityManager, $statsLogger);
+
+        $this->dataWell = $dataWell;
+        $this->api = $api;
+    }
+
+    /**
+     * @{@inheritdoc}
+     */
+    public function load(bool $queue = true, int $limit = null): VendorImportResultMessage
+    {
+        if (!$this->acquireLock()) {
+            return VendorImportResultMessage::error(parent::ERROR_RUNNING);
+        }
+
+        // We're lazy loading the config to avoid errors from missing config values on dependency injection
+        $this->loadConfig();
+
+        $this->queue = $queue;
+        $this->progressStart('Search data well for movies');
+
+        $offset = 1;
+        try {
+            do {
+                // Search the data well for material with acSource set to "comics plus".
+                $query = 'phrase.type="blu-ray"';
+                [$resultArray, $more, $offset] = $this->dataWell->search($query, $offset);
+
+                $pidArray = array_map(function($value) { return ''; }, $resultArray);
+
+                $batchSize = \count($pidArray);
+                $this->updateOrInsertMaterials($pidArray, IdentifierType::PID, $batchSize);
+
+                $event = new ResultEvent($resultArray, IdentifierType::PID, $this->getVendorId());
+                $this->dispatcher->dispatch($event::NAME, $event);
+
+                $this->progressMessageFormatted($this->totalUpdated, $this->totalInserted, $this->totalIsIdentifiers);
+                $this->progressAdvance();
+
+                if ($limit && $this->totalIsIdentifiers >= $limit) {
+                    $more = false;
+                }
+            } while ($more);
+
+            return VendorImportResultMessage::success($this->totalIsIdentifiers, $this->totalUpdated, $this->totalInserted, $this->totalDeleted);
+        } catch (\Exception $exception) {
+            return VendorImportResultMessage::error($exception->getMessage());
+        }
+    }
+
+    /**
+     * Set config fro service from DB vendor object.
+     *
+     * @throws UnknownVendorServiceException
+     * @throws IllegalVendorServiceException
+     */
+    private function loadConfig(): void
+    {
+        // Set the service access configuration from the vendor.
+        $this->dataWell->setSearchUrl($this->getVendor()->getDataServerURI());
+        $this->dataWell->setUser($this->getVendor()->getDataServerUser());
+        $this->dataWell->setPassword($this->getVendor()->getDataServerPassword());
+    }
+}
