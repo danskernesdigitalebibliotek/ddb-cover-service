57a5975570d540cd458c9c29bc7ccab609ad1893
Jesper Kristensen <cableman@linuxdev.dk>
2019-12-21 20:53:57 +0100

    Core: version 1.1.9

--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-(cd php7.3-fpm && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/php7.3-fpm:1.1.8 .)
-(cd cover-service && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service:1.1.8 .)
-(cd nginx && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx:1.1.8 .)
-(cd cover-service-jobs && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service-jobs:1.1.8 .)
+VERSION=1.1.9
 
-docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/php7.3-fpm:1.1.8
-docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service:1.1.8
-docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx:1.1.8
-docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service-jobs:1.1.8
+(cd php7.3-fpm && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/php7.3-fpm:${VERSION} .)
+(cd cover-service && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service:${VERSION} .)
+(cd nginx && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx:${VERSION} .)
+(cd cover-service-jobs && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service-jobs:${VERSION} .)
+
+docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/php7.3-fpm:${VERSION}
+docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service:${VERSION}
+docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx:${VERSION}
+docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service-jobs:${VERSION}
