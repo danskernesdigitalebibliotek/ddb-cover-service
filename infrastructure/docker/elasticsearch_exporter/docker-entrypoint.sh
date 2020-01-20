e92ae572479e7be97db7825487fb2afaf5b342a1
Jesper Kristensen <cableman@linuxdev.dk>
2020-01-20 14:04:43 +0100

    DDBTEAM-381: Moved to use base images

--- infrastructure/docker/elasticsearch_exporter/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -eux
-
-/bin/elasticsearch_exporter  --es.uri=http://${ES_HOST}:${ES_PORT}
--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-(cd php7.3-fpm && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/php7.3-fpm .)
 (cd cover-service && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service .)
 (cd nginx && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx .)
 
-docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/php7.3-fpm:latest
 docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service:latest
 docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx:latest
