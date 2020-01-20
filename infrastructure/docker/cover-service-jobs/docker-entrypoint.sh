06606e3b103febb9ec8300731f6a8f7342394a15
Jesper Kristensen <cableman@linuxdev.dk>
2020-01-20 11:29:43 +0100

    DDBTEAM-381: Removed not needed bundles

--- infrastructure/docker/cover-service-jobs/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -eux
-
-## Run templates with configuration.
-/usr/local/bin/confd --onetime --backend env --confdir /etc/confd
-
-## Start supervisor and the jobs.
-supervisord --nodaemon --configuration /etc/supervisord.conf
--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -3,9 +3,7 @@
 (cd php7.3-fpm && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/php7.3-fpm .)
 (cd cover-service && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service .)
 (cd nginx && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx .)
-(cd cover-service-jobs && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service-jobs .)
 
 docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/php7.3-fpm:latest
 docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service:latest
 docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx:latest
-docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service-jobs:latest
