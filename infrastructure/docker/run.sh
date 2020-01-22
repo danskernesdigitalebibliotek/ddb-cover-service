abfe9ddbc6c8d494cde837612cd9e8e15d116377
Jesper Kristensen <cableman@linuxdev.dk>
2020-01-22 14:38:54 +0100

    DDBTEAM-386: Fixed run script

--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-(cd cover-service && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service .)
-(cd nginx && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx .)
+(cd ../../ && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service .)
+(cd ../../ && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx .)
 
 docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service:latest
 docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx:latest
