12142fd73ec9d7d18e6f25c0d20db364c4d6c995
Jesper Kristensen <cableman@linuxdev.dk>
2020-01-30 14:06:38 +0100

    DDBTEAM-403: Fixed docker local run script

--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-(cd ../../ && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service .)
-(cd ../../ && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx .)
+(cd ../../ && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service --file="infrastructure/docker/cover-service/Dockerfile" .)
+(cd ../../ && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx --file="infrastructure/docker/nginx/Dockerfile" .)
 
 docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service:latest
 docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/nginx:latest
