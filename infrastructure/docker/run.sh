9fd56b8f6ec917dad4f9d5ebf359bf048ead96db
Jesper Kristensen <cableman@linuxdev.dk>
2020-03-10 15:13:01 +0100

    DDBTEAM-389: Updated docker build script

--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -6,5 +6,5 @@ VERSION=latest
 docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=danskernesdigitalebibliotek/cover-service:${VERSION} --file="cover-service/Dockerfile" cover-service
 docker build --no-cache --build-arg VERSION=${VERSION} --tag=danskernesdigitalebibliotek/cover-service-nginx:${VERSION} --file="nginx/Dockerfile" nginx
 
-docker push danskernesdigitalebibliotek/cover-service:latest
-docker push danskernesdigitalebibliotek/cover-service-nginx:latest
+docker push danskernesdigitalebibliotek/cover-service:${VERSION}
+docker push danskernesdigitalebibliotek/cover-service-nginx:${VERSION}
