b5e99975b40d7f1426b09d00bfe9aab50060b7bc
Ture Gjørup <ture@ignatz.dk>
2020-05-12 15:13:04 +0200

    DDBTEAM-467: Fixed prod composer install

--- config/bundles.php
+++ config/bundles.php
@@ -8,7 +8,7 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    FOS\ElasticaBundle\FOSElasticaBundle::class => ['dev' => true, 'test' => true],
+    FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
     Enqueue\Bundle\EnqueueBundle::class => ['all' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
