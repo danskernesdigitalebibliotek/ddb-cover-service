bc6b29cc2406882dc7ab43ef370ac6cb685201f4
Ture Gjørup <ture@ignatz.dk>
2020-06-29 22:18:47 +0200

    DDBTEAM-544: Moved processing to event subscriber

--- src/EventSubscriber/SearchNoHitEventSubscriber.php
+++ src/EventSubscriber/SearchNoHitEventSubscriber.php
@@ -8,8 +8,11 @@ namespace App\EventSubscriber;
 
 use App\Event\SearchNoHitEvent;
 use App\Utils\Message\ProcessMessage;
+use App\Utils\Types\NoHitItem;
 use Enqueue\Client\ProducerInterface;
 use Enqueue\Util\JSON;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -17,21 +20,26 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class SearchNoHitEventSubscriber implements EventSubscriberInterface
 {
+    private $noHitsProcessingEnabled;
     private $producer;
-    private $enabled;
+    private $noHitsCache;
 
     /**
      * SearchNoHitEventSubscriber constructor.
      *
-     * @param producerInterface $producer
-     *   Queue producer to send messages (jobs)
      * @param bool $bindEnableNoHits
      *   Is no hits processing enabled
+     * @param producerInterface $producer
+     *   Queue producer to send messages (jobs)
+     * @param CacheItemPoolInterface $noHitsCache
+     *   Cache pool for storing no hits
      */
-    public function __construct(ProducerInterface $producer, bool $bindEnableNoHits)
+    public function __construct(bool $bindEnableNoHits, ProducerInterface $producer, CacheItemPoolInterface $noHitsCache)
     {
+        $this->noHitsProcessingEnabled = $bindEnableNoHits;
+
         $this->producer = $producer;
-        $this->enabled = $bindEnableNoHits;
+        $this->noHitsCache = $noHitsCache;
     }
 
     /**
@@ -50,17 +58,101 @@ class SearchNoHitEventSubscriber implements EventSubscriberInterface
      * Event handler.
      *
      * @param SearchNoHitEvent $event
+     *   Search no hit event
      */
     public function onSearchNoHitEvent(SearchNoHitEvent $event): void
     {
-        if ($this->enabled) {
-            foreach ($event->getNoHits() as $noHit) {
-                $message = new ProcessMessage();
-                $message->setIdentifierType($noHit->getIsType())
-                    ->setIdentifier($noHit->getIsIdentifier());
+        if ($this->noHitsProcessingEnabled) {
+            $keyedNoHits = [];
 
-                $this->producer->sendEvent('SearchNoHitsTopic', JSON::encode($message));
+            /** @var NoHitItem $noHit */
+            foreach ($event->getNoHits() as $noHit) {
+                $cacheKey = $this->getValidCacheKey($noHit->getIsType(), $noHit->getIsIdentifier());
+                $keyedNoHits[$cacheKey] = $noHit;
             }
+
+            $cacheKeys = array_keys($keyedNoHits);
+            $nonCommittedCacheItems = $this->getNonCachedNoHits($cacheKeys);
+            $this->sendSearchNoHitEvents($nonCommittedCacheItems);
         }
+    }
+
+    /**
+     * Send search no hit events.
+     *
+     * @param array $nonCommittedCacheItems
+     *   Array of cache items
+     */
+    private function sendSearchNoHitEvents(array $nonCommittedCacheItems): void
+    {
+        foreach ($nonCommittedCacheItems as $cacheItem) {
+            /** @var NoHitItem $noHitItem */
+            $noHitItem = $cacheItem->get();
+            $message = new ProcessMessage();
+            $message->setIdentifierType($noHitItem->getIsType())
+                ->setIdentifier($noHitItem->getIsIdentifier());
+
+            $this->noHitsCache->saveDeferred($cacheItem);
+
+            $this->producer->sendEvent('SearchNoHitsTopic', JSON::encode($message));
+        }
+
+        $this->noHitsCache->commit();
+    }
+
+    /**
+     * Get cache items for the identifiers not present in the cache.
+     *
+     * @param array $keyedNoHits
+     *   Array og cacheKey => NoHitItem pairs
+     *
+     * @return array
+     *   Array of cache items not yet committed to cache
+     */
+    private function getNonCachedNoHits(array $keyedNoHits): array
+    {
+        $nonCommittedCacheItems = [];
+        try {
+            $keys = array_keys($keyedNoHits);
+            $cacheItems = $this->noHitsCache->getItems($keys);
+            foreach ($cacheItems as $cacheItem) {
+                if (!$cacheItem->isHit()) {
+                    /** @var NoHitItem $noHitItem */
+                    $noHitItem = $keyedNoHits[$cacheItem->getKey()];
+                    $cacheItem->set($noHitItem);
+                    $nonCommittedCacheItems[] = $cacheItem;
+                }
+            }
+        } catch (InvalidArgumentException $e) {
+            // @TODO Logging?
+        }
+
+        return $nonCommittedCacheItems;
+    }
+
+    /**
+     * Get an array of valid cache keys for the identifiers.
+     *
+     * Keys should only contain letters (A-Z, a-z), numbers (0-9) and the _ and . symbols.
+     *
+     * @see https://www.php-fig.org/psr/psr-6/
+     * @see https://symfony.com/doc/current/components/cache/cache_items.html#cache-item-keys-and-values
+     *
+     * @param string $type
+     *   The identifier type
+     * @param string $identifier
+     *   The identifier
+     *
+     * @return string
+     *   The cache key
+     */
+    private function getValidCacheKey(string $type, string $identifier): string
+    {
+        if (IdentifierType::PID === $type) {
+            $identifier = str_replace(':', '_', $identifier);
+            $identifier = str_replace('-', '_', $identifier);
+        }
+
+        return $key = $type.'.'.$identifier;
     }
 }
--- src/Service/NoHitService.php
+++ src/Service/NoHitService.php
@@ -8,11 +8,7 @@
 namespace App\Service;
 
 use App\Event\SearchNoHitEvent;
-use App\Utils\Types\IdentifierType;
 use App\Utils\Types\NoHitItem;
-use Psr\Cache\CacheItemInterface;
-use Psr\Cache\CacheItemPoolInterface;
-use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpKernel\Event\TerminateEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -22,26 +18,22 @@ use Symfony\Component\HttpKernel\KernelEvents;
  */
 final class NoHitService
 {
-    private $noHitsProcessingEnabled;
     private $dispatcher;
     private $metricsService;
-    private $noHitsCache;
+    private $noHitsProcessingEnabled;
 
     /**
      * NoHitService constructor.
      *
-     * @param bool $bindEnableNoHits
      * @param EventDispatcherInterface $dispatcher
      * @param MetricsService $metricsService
-     * @param CacheItemPoolInterface $noHitsCache
+     * @param bool $bindEnableNoHits
      */
-    public function __construct(bool $bindEnableNoHits, EventDispatcherInterface $dispatcher, MetricsService $metricsService, CacheItemPoolInterface $noHitsCache)
+    public function __construct(EventDispatcherInterface $dispatcher, MetricsService $metricsService, bool $bindEnableNoHits)
     {
-        $this->noHitsProcessingEnabled = $bindEnableNoHits;
-
         $this->dispatcher = $dispatcher;
         $this->metricsService = $metricsService;
-        $this->noHitsCache = $noHitsCache;
+        $this->noHitsProcessingEnabled = $bindEnableNoHits;
     }
 
     /**
@@ -56,91 +48,10 @@ final class NoHitService
      */
     public function handleSearchNoHits(string $type, array $requestIdentifiers, array $foundIdentifiers): void
     {
-        if ($this->noHitsProcessingEnabled) {
-            $notFoundIdentifiers = array_diff($requestIdentifiers, $foundIdentifiers);
-            $notFoundKeyIdentifierArray = $this->getKeyIdentifierArray($type, $notFoundIdentifiers);
+        $notFoundIdentifiers = array_diff($requestIdentifiers, $foundIdentifiers);
 
-            $nonCachedIdentifierItems = $this->getNonCachedNoHits($notFoundKeyIdentifierArray);
-            $this->dispatchNoHits($type, ...$nonCachedIdentifierItems);
-
-            $this->metricsService->counter('no_hit_event_duration_seconds', 'Total number of no-hits', count($notFoundIdentifiers), ['type' => 'rest']);
-        }
-    }
-
-    /**
-     * Get array identifiers key by unique cache key.
-     *
-     * @param string $type
-     *   The type ('pid', 'isbn', etc) of identifiers given
-     * @param array $identifiers
-     *   Array of identifiers of {type}
-     *
-     * @return array
-     *   An array og cacheKey => value pairs
-     */
-    private function getKeyIdentifierArray(string $type, array $identifiers): array
-    {
-        $result = [];
-        foreach ($identifiers as $identifier) {
-            $key = $this->getValidCacheKey($type, $identifier);
-            $result[$key] = $identifier;
-        }
-
-        return $result;
-    }
-
-    /**
-     * Get an array of valid cache keys for the identifiers.
-     *
-     * Keys should only contain letters (A-Z, a-z), numbers (0-9) and the _ and . symbols.
-     *
-     * @see https://www.php-fig.org/psr/psr-6/
-     * @see https://symfony.com/doc/current/components/cache/cache_items.html#cache-item-keys-and-values
-     *
-     * @param string $type
-     *   The identifier type
-     * @param string $identifier
-     *   The identifier
-     *
-     * @return string
-     *   The cache key
-     */
-    private function getValidCacheKey(string $type, string $identifier): string
-    {
-        if (IdentifierType::PID === $type) {
-            $identifier = str_replace(':', '_', $identifier);
-            $identifier = str_replace('-', '_', $identifier);
-        }
-
-        return $key = $type.'.'.$identifier;
-    }
-
-    /**
-     * Get cache items for the identifiers not present in the cache.
-     *
-     * @param array $keyIdentifierArray
-     *   Array og cacheKey => value pairs
-     *
-     * @return array
-     *   Array of cache items with no hits in the cache
-     */
-    private function getNonCachedNoHits(array $keyIdentifierArray): array
-    {
-        $nonCachedIdentifierItems = [];
-        try {
-            $keys = array_keys($keyIdentifierArray);
-            $items = $this->noHitsCache->getItems($keys);
-            foreach ($items as $item) {
-                if (!$item->isHit()) {
-                    $item->set($keyIdentifierArray[$item->getKey()]);
-                    $nonCachedIdentifierItems[] = $item;
-                }
-            }
-        } catch (InvalidArgumentException $e) {
-            // @TODO Logging?
-        }
-
-        return $nonCachedIdentifierItems;
+        $this->metricsService->counter('no_hit_event_duration_seconds', 'Total number of no-hits', count($notFoundIdentifiers), ['type' => 'rest']);
+        $this->dispatchNoHits($type, $notFoundIdentifiers);
     }
 
     /**
@@ -148,21 +59,17 @@ final class NoHitService
      *
      * @param string $type
      *   The type ('pid', 'isbn', etc) of identifiers given
-     * @param CacheItemInterface ...$identifierItems
-     *   A list of identifier cache items
+     * @param array $identifiers
+     *   Array of no hit identifiers of {type}
      */
-    private function dispatchNoHits(string $type, CacheItemInterface ...$identifierItems): void
+    private function dispatchNoHits(string $type, array $identifiers): void
     {
-        if (!empty($identifierItems)) {
+        if ($this->noHitsProcessingEnabled && !empty($identifiers)) {
             $noHits = [];
 
-            foreach ($identifierItems as $identifierItem) {
-                $noHits[] = new NoHitItem($type, $identifierItem->get());
-
-                $this->noHitsCache->saveDeferred($identifierItem);
+            foreach ($identifiers as $identifier) {
+                $noHits[] = new NoHitItem($type, $identifier);
             }
-
-            $this->noHitsCache->commit();
 
             $this->dispatcher->addListener(
                 KernelEvents::TERMINATE,
