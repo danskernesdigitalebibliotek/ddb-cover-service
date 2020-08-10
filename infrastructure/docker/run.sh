235e99f79cdeaf1923a5616accfc630492c7bf9a
Jesper Kristensen <cableman@linuxdev.dk>
2020-08-10 10:55:11 +0200

    DDBTEAM-563: Bumped 1.x version in build files

--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-(cd ../../ && docker build --no-cache --tag=danskernesdigitalebibliotek/cover-service:1.2.14 --file="infrastructure/docker/cover-service/Dockerfile" .)
-(cd ../../ && docker build --no-cache --tag=danskernesdigitalebibliotek/cover-service-nginx:1.2.14 --file="infrastructure/docker/nginx/Dockerfile" .)
+(cd ../../ && docker build --no-cache --tag=danskernesdigitalebibliotek/cover-service:1.2.15 --file="infrastructure/docker/cover-service/Dockerfile" .)
+(cd ../../ && docker build --no-cache --tag=danskernesdigitalebibliotek/cover-service-nginx:1.2.15 --file="infrastructure/docker/nginx/Dockerfile" .)
 
-docker push danskernesdigitalebibliotek/cover-service:1.2.14
-docker push danskernesdigitalebibliotek/cover-service-nginx:1.2.14
+docker push danskernesdigitalebibliotek/cover-service:1.2.15
+docker push danskernesdigitalebibliotek/cover-service-nginx:1.2.15
