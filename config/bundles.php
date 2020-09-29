5d02417619a05a78efba52f65a4429edc54e256a
Jesper Kristensen <cableman@linuxdev.dk>
2020-09-29 21:58:56 +0200

    DDBTEAM-640: Updated no-hits into messager with amqp

--- config/bundles.php
+++ config/bundles.php
@@ -9,7 +9,6 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
-    Enqueue\Bundle\EnqueueBundle::class => ['all' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
