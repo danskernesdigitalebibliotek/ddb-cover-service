ecdd6c616e0861d6c9b0b0ea4691a8b6aa04a4a1
Jesper Kristensen <cableman@linuxdev.dk>
2022-07-05 14:43:56 +0200

    DDBTEAM-948: Fixed code style analyze

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,11 +3,16 @@
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
     ->exclude('var')
+    ->notPath('Api/Dto/Cover.php')
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        '@Symfony:risky' => false,
+        'phpdoc_align' => false,
+        'no_superfluous_phpdoc_tags' => false,
+        'array_syntax' => ['syntax' => 'short'],
     ])
     ->setFinder($finder)
     ->setCacheFile('.php-cs-fixer.cache') // forward compatibility with 3.x line
