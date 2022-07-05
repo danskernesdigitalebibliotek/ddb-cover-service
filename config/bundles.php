2e73b0c23cf8d4c1608ccca00ac9b737ecacd58f
Jesper Kristensen <cableman@linuxdev.dk>
2022-07-05 14:47:43 +0200

    DDBTEAM-948: Fixed controller annotations

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     DanskernesDigitaleBibliotek\AgencyAuthBundle\DanskernesDigitaleBibliotekAgencyAuthBundle::class => ['all' => true],
     ItkDev\MetricsBundle\ItkDevMetricsBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
