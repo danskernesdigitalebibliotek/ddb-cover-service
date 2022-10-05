2bbca75336229ecf797167eddf3bd46834ee4cbb
Jesper Kristensen <cableman@linuxdev.dk>
2022-10-05 21:21:27 +0200

    DDBTEAM-1060: Switch landing page to new nginx base image

--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -5,8 +5,8 @@ VERSION=alpha
 
 docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=danskernesdigitalebibliotek/cover-service:${VERSION} --file="cover-service/Dockerfile" cover-service
 docker build --no-cache --build-arg VERSION=${VERSION} --tag=danskernesdigitalebibliotek/cover-service-nginx:${VERSION} --file="nginx/Dockerfile" nginx
-#docker build --no-cache --tag=danskernesdigitalebibliotek/cover-service-landing:${VERSION} --file="cover-service-landing/Dockerfile" cover-service-landing
+docker build --no-cache --tag=danskernesdigitalebibliotek/cover-service-landing:${VERSION} --file="cover-service-landing/Dockerfile" cover-service-landing
 
 docker push danskernesdigitalebibliotek/cover-service:${VERSION}
 docker push danskernesdigitalebibliotek/cover-service-nginx:${VERSION}
-#docker push danskernesdigitalebibliotek/cover-service-landing:${VERSION}
+docker push danskernesdigitalebibliotek/cover-service-landing:${VERSION}
